e6295b09dc813ad14e5c515d30b8bb4caca9f9cd
Povilasss <povilas.nanartonis.pn@gmail.com>
2024-10-29 22:25:58 +0200

    Logout mygtukas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,20 +7,45 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-<nav>
-    <!-- Navigacijos juosta -->
-    <ul>
-        <li><a href="{{ route('home') }}">Pagrindinis</a></li>
-        <li><a href="{{ route('client.index') }}">Kliento posistemis</a></li>
-        <li><a href="{{ route('employee.dashboard') }}">Darbuotojo posistemis</a></li>
-        <li><a href="{{ route('admin.dashboard') }}">Administratoriaus posistemis</a></li>
-    </ul>
+<!-- Navigacijos juosta -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">Sistemos pavadinimas</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('home') }}">Pagrindinis</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('client.index') }}">Kliento posistemis</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('employee.dashboard') }}">Darbuotojo posistemis</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.dashboard') }}">Administratoriaus posistemis</a>
+                </li>
+            </ul>
+
+            <!-- Naudotojo informacija ir Logout mygtukas -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <span class="nav-link">Jonas Jonaitis</span> <!-- Čia naudotojo vardas ir pavardė -->
+                </li>
+                <li class="nav-item">
+                    <button class="btn btn-outline-secondary" disabled>Logout</button> <!-- Išjungtas Logout mygtukas -->
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
 
-<div class="container">
+<!-- Pagrindinis turinys -->
+<div class="container mt-4">
     @yield('content')
 </div>
 
 <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
+
